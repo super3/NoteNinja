72d06d16424265cce2e4e6d743258f541aa61f6d
Shawn Wilkinson <me@super3.org>
2012-11-13 15:17:57 -0500

    Photo Page

--- NoteNinja/Photo.xaml.cs
+++ NoteNinja/Photo.xaml.cs
@@ -19,9 +19,9 @@ namespace NoteNinja
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class PhotoPage : Page
     {
-        public MainPage()
+        public PhotoPage()
         {
             this.InitializeComponent();
         }
